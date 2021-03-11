bb5d15b126a1c5108f5618226f1378f6e7e83dca
Tovio Roberts <clownfragment@gmail.com>
2021-03-10 20:08:10 -0700

    Auto Updated: Wed 08:20 Mar 10 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -33,15 +33,15 @@ farmhouse = [4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0
 
 
 def median(lst):
-    pass
+    lst_sorted = sorted(lst)
 
 
 
 odd_list = [13, 18, 13, 14, 13, 16, 14, 21, 13]
 even_list = [15, 14, 10, 8, 12, 8, 16, 13]
 
-print(odd_list)
+print(sorted(odd_list))
 print(median(odd_list))
 print()
-print(even_list)
+print(sorted(even_list))
 print(median(even_list))
