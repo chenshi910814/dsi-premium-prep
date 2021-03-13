50bc6c2683dcc7b5e6c0170f1112b49a07c64f12
Tovio Roberts <clownfragment@gmail.com>
2021-03-13 13:42:41 -0700

    Auto Updated: Sat 42:13 Mar 13 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -177,7 +177,7 @@ test_outliers.append(90_000)
 test_outliers.append(90_000)
 test_outliers.append(5_000)
 
-print(detect_outliers(test_outliers, outlier_coef=500))
+print(detect_outliers(test_outliers, outlier_coef=1.5))
 print(remove_outliers(test_outliers))
 
 
