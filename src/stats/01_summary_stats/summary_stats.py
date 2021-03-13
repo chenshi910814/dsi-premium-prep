4bf7841f52d571d61d593c34faa27c91ef517efd
Tovio Roberts <clownfragment@gmail.com>
2021-03-13 12:52:25 -0700

    Auto Updated: Sat 52:12 Mar 13 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -108,3 +108,11 @@ odd_test_dat = list(range(0, 100+1))
 
 
 
+a = [15,2,9,5,6,7,27,12,18,19,1]
+b = [6,1,4,51,7,16,10,14,46,22,24,56,48,54]
+
+
+print(sorted(a))
+print(five_number_summary(a))
+print(mean(a))
+print(median(a))
