682ce73c924516135e2e479c8382ac275f24eedc
Tovio Roberts <clownfragment@gmail.com>
2021-03-17 20:50:18 -0600

    Auto Updated: Wed 50:20 Mar 17 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -29,3 +29,16 @@ for pet_outcome in pet_outcomes:
 # print(len(two_or_more_cats) / len(pet_outcomes))
 
 
+from random import choice
+
+def coin_flip():
+    flip = ['H', 'T']
+    return choice(flip)
+
+def series_of_flips(n):
+    flips = []
+    for _ in range(n):
+        flips.append(coin_flip())
+    return flips
+
+print(series_of_flips(12))
