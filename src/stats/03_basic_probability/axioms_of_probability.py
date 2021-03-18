c9df778005b6a348ec7c1078514540ffe6837ce4
Tovio Roberts <clownfragment@gmail.com>
2021-03-17 19:43:59 -0600

    Auto Updated: Wed 43:19 Mar 17 2021

--- src/stats/03_basic_probability/axioms_of_probability.py
+++ src/stats/03_basic_probability/axioms_of_probability.py
@@ -7,11 +7,24 @@ AB = BA
 set1 = {'a', 'b', 'c'}
 set2 = {'c', 'd', 'e'}
 
-print(set1.union(set2) == set2.union(set1))
-print(set1.intersection(set2) == set2.intersection(set1))
+# print(set1.union(set2) == set2.union(set1))
+# print(set1.intersection(set2) == set2.intersection(set1))
 
 a = True
 b = False
 
-print( (a or b) == (b or a))
-print( (a and b) == (b and a))
+# print( (a or b) == (b or a))
+# print( (a and b) == (b and a))
+
+
+'''
+Associative
+'''
+set1 = {'a', 'b', 'c'}
+set2 = {'c', 'd', 'e'}
+set3 = {'a', 'e', 'f'}
+
+# print( (set1.union(set2)).union(set3) == (set3.union(set2)).union(set1) ) # --> True
+# print( (set1.intersection(set2)).intersection(set3) == (set3.intersection(set2)).intersection(set1)) # --> True
+
+
