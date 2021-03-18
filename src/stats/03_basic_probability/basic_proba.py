04f2a6cb0703ae9a232f9a603c29a193eceb464f
Tovio Roberts <clownfragment@gmail.com>
2021-03-17 20:40:52 -0600

    Auto Updated: Wed 40:20 Mar 17 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -13,5 +13,14 @@ for pet1 in pets:
             for pet4 in pets:
                 pet_outcomes.append([pet1, pet2, pet3, pet4])
 
+# for pet_outcome in pet_outcomes:
+#     print(pet_outcome)
+
+two_or_more_cats = []
+
 for pet_outcome in pet_outcomes:
-    print(pet_outcome)
+    if pet_outcome.count('cat') >= 2:
+        two_or_more_cats.append(pet_outcome)
+
+
+print(len(two_or_more_cats) / len(pet_outcomes))
