5f4113891752c2aea6d14f12a8fce4d9d3e956bb
Tovio Roberts <clownfragment@gmail.com>
2021-03-23 19:12:22 -0600

    Auto Updated: Tue 12:19 Mar 23 2021

--- src/stats/05_general_analytic_approach/analytic_approach.py
+++ src/stats/05_general_analytic_approach/analytic_approach.py
@@ -1,2 +1,20 @@
 
 rolls = range(1, 36+1)
+
+suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
+card_nums = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
+cards = []
+for suit in suits:
+    for card in card_nums:
+        cards.append(f'{card} of {suit}')
+
+for card in cards:
+    print(card)
+
+flip = ['T', 'H']
+coin_flips = []
+for flip1 in flip:
+    for flip2 in flip:
+        for flip3 in flip:
+            coin_flips.append([flip1, flip2, flip3])
+
