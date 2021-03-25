6377d3f18d608f48db31e738ed3de41ccd537367
Tovio Roberts <clownfragment@gmail.com>
2021-03-24 19:15:54 -0600

    Auto Updated: Wed 15:19 Mar 24 2021

--- src/stats/06_discrete_distributions/bernoulli.py
+++ src/stats/06_discrete_distributions/bernoulli.py
@@ -1,4 +1,13 @@
 from random import random
 
 def bernoulli(p_success):
-    
+    draw = random()
+
+    print(p_success, draw)
+    if draw < p_success:
+        return 1
+    else:
+        return 0
+
+
+bernoulli(p_success=0.3789546)
