116078a98fb7e68a78ddbc37683967788803d9da
Tovio Roberts <clownfragment@gmail.com>
2021-03-24 19:26:29 -0600

    Auto Updated: Wed 26:19 Mar 24 2021

--- src/stats/06_discrete_distributions/bernoulli.py
+++ src/stats/06_discrete_distributions/bernoulli.py
@@ -20,11 +20,3 @@ for _ in range(trials):
 
 # print(round(true_count / trials, 4))
 
-
-binary = [0, 1]
-
-for bit1 in binary:
-    for bit2 in binary:
-        for bit3 in binary:
-            for bit4 in binary:
-                print([bit1, bit2, bit3, bit4])
---
+++ src/stats/06_discrete_distributions/binomial.py
@@ -0,0 +1,8 @@
+
+binary = [0, 1]
+
+for bit1 in binary:
+    for bit2 in binary:
+        for bit3 in binary:
+            for bit4 in binary:
+                print([bit1, bit2, bit3, bit4])
