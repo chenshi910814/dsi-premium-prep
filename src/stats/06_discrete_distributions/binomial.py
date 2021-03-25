fe46f0800d34b9d415d90cd4111ff8c3291240c2
Tovio Roberts <clownfragment@gmail.com>
2021-03-24 19:27:34 -0600

    Auto Updated: Wed 27:19 Mar 24 2021

--- src/stats/06_discrete_distributions/binomial.py
+++ src/stats/06_discrete_distributions/binomial.py
@@ -7,3 +7,6 @@ for bit1 in binary:
         for bit3 in binary:
             for bit4 in binary:
                 four_bit_binary.append([bit1, bit2, bit3, bit4])
+
+for bin_ in four_bit_binary:
+    print(bin_)
