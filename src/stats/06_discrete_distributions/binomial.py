f292f1aca3165bf79a18c52dadec0aa5bf621b72
Tovio Roberts <clownfragment@gmail.com>
2021-03-25 20:21:10 -0600

    Auto Updated: Thu 21:20 Mar 25 2021

--- src/stats/06_discrete_distributions/binomial.py
+++ src/stats/06_discrete_distributions/binomial.py
@@ -263,7 +263,11 @@ print(round(binomial_pmf(n, p, k), 4))
 
 
 # Sitting on a park bench you observe geese walking by. There's a probability of 0.3 that any goose walking by has black feet. What is the probability that 4 out of the next 12 geese walking by has black feet?
-# n = ? k = ? p = ?
+n = 12 
+k = 4
+p = 0.3
+
+print(round(binomial_pmf(n, p, k), 4))
 
 # At the cat cafe, there is a 40% chance that any one cat you see is a Siberian. What is the probability that 6 out of the 14 cats at the park are Siberian?
 # n = ? k = ? p = ?
