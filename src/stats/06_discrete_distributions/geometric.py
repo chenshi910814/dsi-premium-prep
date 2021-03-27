e09974aa0cb21078710b3dc8b47f1d26cf656fcf
Tovio Roberts <clownfragment@gmail.com>
2021-03-27 13:56:10 -0600

    Auto Updated: Sat 56:13 Mar 27 2021

--- src/stats/06_discrete_distributions/geometric.py
+++ src/stats/06_discrete_distributions/geometric.py
@@ -16,4 +16,23 @@ What is the probability that a given packet of data is lost in the 15th router?
 p_lost = 0.01
 k = 15
 
-print(geometric_pmf(p_lost, k))
+# print(geometric_pmf(p_lost, k))
+
+
+'''
+You have a series of routers transmitting packets of data. There is a 0.99 probability that a given packet of data passes through the router.
+
+What is the probability that a given packet of data is lost in or before the 15th router?
+'''
+
+p_lost = 0.01
+k = 15
+
+def geometric_cdf(p, k_high):
+    accum = 0.0
+    for k in range(0, k_high+1):
+        accum += geometric_pmf(p, k)
+    return accum
+
+print(geometric_cdf(p_lost, k))
+
