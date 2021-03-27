cf1a21478b70f8cf846f67640c856ad2a25fec96
Tovio Roberts <clownfragment@gmail.com>
2021-03-27 12:29:36 -0600

    Auto Updated: Sat 29:12 Mar 27 2021

--- src/stats/06_discrete_distributions/poisson.py
+++ src/stats/06_discrete_distributions/poisson.py
@@ -2,3 +2,11 @@ from math import e, factorial
 
 def poisson_pmf(lam, k):
     return (e**(-lam) * lam**k) / factorial(k)
+
+'''
+On average, ten people visit the ATM in an hour. What is the probability that 12 people visit the ATM in an hour?
+'''
+lam = 10
+k = 12
+
+print(poisson_pmf(lam, k))
