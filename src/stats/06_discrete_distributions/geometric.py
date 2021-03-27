34eb3f48bc8a70b613ee0d6bd4153acd82bdc031
Tovio Roberts <clownfragment@gmail.com>
2021-03-27 13:54:00 -0600

    Auto Updated: Sat 54:13 Mar 27 2021

--- src/stats/06_discrete_distributions/geometric.py
+++ src/stats/06_discrete_distributions/geometric.py
@@ -2,7 +2,18 @@
 def geometric_pmf(p, k):
     return p * (1-p)**(k-1)
 
-for k in range(1, 20):
-    p = 0.5
+# for k in range(1, 20):
+#     p = 0.5
 
-    print(geometric_pmf(p, k))
+#     print(geometric_pmf(p, k))
+
+
+'''
+You have a series of routers transmitting packets of data. There is a 0.99 probability that a given packet of data passes through the router.
+
+What is the probability that a given packet of data is lost in the 15th router?
+'''
+p_lost = 0.01
+k = 15
+
+print(geometric_pmf(p_lost, k))
