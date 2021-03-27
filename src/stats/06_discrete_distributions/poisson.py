0b00695e2b89b52d518e347d781dca6a2098ec08
Tovio Roberts <clownfragment@gmail.com>
2021-03-27 12:33:07 -0600

    Auto Updated: Sat 33:12 Mar 27 2021

--- src/stats/06_discrete_distributions/poisson.py
+++ src/stats/06_discrete_distributions/poisson.py
@@ -9,4 +9,12 @@ On average, ten people visit the ATM in an hour. What is the probability that 12
 lam = 10
 k = 12
 
+# print(poisson_pmf(lam, k))
+
+'''
+A specific space telescope takes random images of deep space. On average 23 Super Giant stars are seen in an image. What is the probability that two randomly selected images will have a total count of 35 Super Giant stars in them?
+'''
+lam = 23 * 2
+k = 35
+
 print(poisson_pmf(lam, k))
