c97083aa76f57228f42bed932c1b0789b5868cef
Tovio Roberts <clownfragment@gmail.com>
2021-03-30 19:54:45 -0600

    Auto Updated: Tue 54:19 Mar 30 2021

--- src/stats/07_continuous_distributions/uniform.py
+++ src/stats/07_continuous_distributions/uniform.py
@@ -36,3 +36,11 @@ def exponential_variance(lam):
 def exponential_std(lam):
     return sqrt(exponential_variance(lam))
 
+
+
+lam = 0.1
+
+print(1 - exponential_cdf(lam, x))
+print(exponential_mean(lam))
+print(exponential_variance(lam))
+print(exponential_std(lam))
