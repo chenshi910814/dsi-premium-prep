9b0c25410fb563fefd7a906bdcc913156ba403c2
Tovio Roberts <clownfragment@gmail.com>
2021-03-30 20:18:09 -0600

    Auto Updated: Tue 18:20 Mar 30 2021

--- src/stats/07_continuous_distributions/uniform.py
+++ src/stats/07_continuous_distributions/uniform.py
@@ -19,29 +19,3 @@ def get_float(num_bits=8):
     
 # print(get_binary())
 
-def exponential_pdf(lam, x):
-    if x < 0: return 0
-    return lam * e**(-lam * x)
-
-def exponential_cdf(lam, x):
-    if x < 0: return 0
-    return 1 - e**(-lam * x)
-
-def exponential_mean(lam):
-    return 1 / lam
-
-def exponential_variance(lam):
-    return 1 / lam**2
-
-def exponential_std(lam):
-    return sqrt(exponential_variance(lam))
-
-
-
-lam = 0.1
-
-# print(1 - exponential_cdf(lam, 10))
-# print(exponential_mean(lam))
-# print(exponential_variance(lam))
-# print(exponential_std(lam))
-
