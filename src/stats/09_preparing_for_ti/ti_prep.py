3e8ffce87dcacd71b97b5aa616f4f85d271a1d50
Tovio Roberts <clownfragment@gmail.com>
2021-04-03 13:25:11 -0600

    Auto Updated: Sat 25:13 Apr 03 2021

--- src/stats/09_preparing_for_ti/ti_prep.py
+++ src/stats/09_preparing_for_ti/ti_prep.py
@@ -5,10 +5,23 @@ from random import random
 
 def get_result(prior_res=0):
     rand = random()
-    if rand < (0.33 - prior_result * 0.1):
+    if rand < (0.33 - prior_res * 0.1):
         return 0
-    elif rand < 0.66 + prior_result * 0.1:
+    elif rand < 0.66 + prior_res * 0.1:
         return 1
     else:
         return 2
 
+def get_trials(n=12):
+    res = get_result()
+    lst = [res]
+
+    for _ in range(n-1):
+        res = get_result(res)
+        lst.append(res)
+
+    return lst
+
+
+
+print(get_trials(n=12))
