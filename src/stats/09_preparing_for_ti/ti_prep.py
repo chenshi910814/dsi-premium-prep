058ffc692a45c980bd3b371914458e3c89ab7ac5
Tovio Roberts <clownfragment@gmail.com>
2021-04-03 13:19:57 -0600

    Auto Updated: Sat 19:13 Apr 03 2021

--- src/stats/09_preparing_for_ti/ti_prep.py
+++ src/stats/09_preparing_for_ti/ti_prep.py
@@ -1,2 +1,13 @@
+from random import random
+
 # Dependent Sampling
 
+
+def get_result():
+    rand = random()
+    if rand < 0.33:
+        return 0
+    elif rand < 0.66:
+        return 1
+    else:
+        return 2
