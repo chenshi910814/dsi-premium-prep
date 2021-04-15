ea5fbe815206c020ed7faf481aa2e0e2eef144a1
Clark Walker <heartbeast@pop-os.localdomain>
2021-04-14 21:06:21 -0600

    uploading day 3

--- src/python/main.py
+++ src/python/main.py
@@ -7,8 +7,8 @@
 def main():
     print("\n"*20)
     # import day_01_lib
-    import day_02_lib
-    # import day_03_lib
+    # import day_02_lib
+    import day_03_lib
     # import day_04_lib
     # import day_05_lib
     # import day_06_lib
