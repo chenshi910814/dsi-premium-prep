94917b820b194e8470d6389b5cb493b7bb3d3e34
Tovio Roberts <clownfragment@gmail.com>
2021-04-24 12:56:57 -0600

    Auto Updated: Sat 56:12 Apr 24 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -159,4 +159,4 @@ def detect_outliers(lst, outlier_coef=1.5):
 test_outliers = list(range(0,100))
 test_outliers.append(10_000)
 
-print(detect_outliers(test_outliers, outlier_coef=1.5))
+print(detect_outliers(test_outliers, outlier_coef=7.5))
