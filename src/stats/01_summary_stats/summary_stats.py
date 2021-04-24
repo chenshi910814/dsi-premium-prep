41eb119379f2c70f4721bf652afa93fcaa5f7499
Tovio Roberts <clownfragment@gmail.com>
2021-04-24 12:56:24 -0600

    Auto Updated: Sat 56:12 Apr 24 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -142,3 +142,21 @@ b = list(range(0, 100+1, 5))
 # print(iqr(b))
 
 
+def detect_outliers(lst, outlier_coef=1.5):
+    _, q1, _, q3, _ = five_number_summary(lst)
+    iqr_ = iqr(lst)    
+
+    outliers = []
+
+    for num in lst:
+        if num < q1 - outlier_coef*iqr_:
+            outliers.append(num)
+        if num > q3 + outlier_coef*iqr_:
+            outliers.append(num)
+    
+    return outliers
+        
+test_outliers = list(range(0,100))
+test_outliers.append(10_000)
+
+print(detect_outliers(test_outliers, outlier_coef=1.5))
