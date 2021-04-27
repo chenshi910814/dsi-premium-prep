d083c145c3f433d9197bd369b3466aa446c7f295
Clark Walker <303walker@gmail.com>
2021-04-26 18:18:52 -0600

    uploading day 8

--- src/python/main.py
+++ src/python/main.py
@@ -18,8 +18,8 @@ def main():
     # import day_05_lib
     # import day_06_lib
     # import day_07_lib
-    import day_08_lib
-    # import day_09_lib
+    # import day_08_lib
+    import day_09_lib
     # import day_10_lib
     # import day_11_lib
     # import day_12_lib
