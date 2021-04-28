5e6ba9c3b12401a102d204e70f4e68453e630c61
Tovio Roberts <clownfragment@gmail.com>
2021-04-27 20:50:25 -0600

    Auto Updated: Tue 50:20 Apr 27 2021

--- src/stats/02_sets/sets.py
+++ src/stats/02_sets/sets.py
@@ -174,4 +174,4 @@ for outcome in sample_space:
 '''
 3. List the sample points which are in the Union of events A and B from above
 '''
-print(union(A, B))
+print(len(union(A, B)) / len(sample_space))
