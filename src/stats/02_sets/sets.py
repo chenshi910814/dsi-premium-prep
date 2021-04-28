1a7c04cb7f71b1f5e941ce4dff6b7b243c0761f8
Tovio Roberts <clownfragment@gmail.com>
2021-04-27 19:24:41 -0600

    Auto Updated: Tue 24:19 Apr 27 2021

--- src/stats/02_sets/sets.py
+++ src/stats/02_sets/sets.py
@@ -3,4 +3,16 @@
 lst = ['kayaking', 'tennis', 'rolf ball', 'swimming', 'tennis', 'kayaking']
 
 lst = list(set(lst))
-print(lst) # --> ['swimming', 'tennis', 'kayaking', 'rolf ball']
+# print(lst) # --> ['swimming', 'tennis', 'kayaking', 'rolf ball']
+
+
+def dedupe_in_order(lst):
+    deduped = []
+
+    for element in lst:
+        if element not in deduped:
+            deduped.append(element)
+    
+    return deduped
+
+print(dedupe_in_order(lst))
