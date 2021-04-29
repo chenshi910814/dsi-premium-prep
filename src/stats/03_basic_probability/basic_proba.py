d72915a79a362a2bc30c27dbb4d263cc99300902
Tovio Roberts <clownfragment@gmail.com>
2021-04-28 19:57:34 -0600

    Auto Updated: Wed 57:19 Apr 28 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -8,23 +8,23 @@ for pet1 in pets:
             for pet4 in pets:
                 pet_outcomes.append([pet1, pet2, pet3, pet4])
 
-for pet_outcome in pet_outcomes:
-    print(pet_outcome)
+# for pet_outcome in pet_outcomes:
+#     print(pet_outcome)
 
 # print(4**4)
 # print(len(pet_outcomes))
 
 two_or_more_c = []
 
-for pet_outcome in pet_outcomes:
-    if pet_outcome.count('c') >= 2:
-        two_or_more_c.append(pet_outcome)
+# for pet_outcome in pet_outcomes:
+#     if pet_outcome.count('c') >= 2:
+#         two_or_more_c.append(pet_outcome)
 
 # print(two_or_more_c)
 
-card_A = len(two_or_more_c)
-card_S = len(pet_outcomes)
+# card_A = len(two_or_more_c)
+# card_S = len(pet_outcomes)
 
-proba_two_or_more_c = card_A / card_S
+# proba_two_or_more_c = card_A / card_S
 
-print(proba_two_or_more_c)
+# print(proba_two_or_more_c)
