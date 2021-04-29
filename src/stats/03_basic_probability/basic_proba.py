0ba920a0246c6753b5814291bd186f051131eb81
Tovio Roberts <clownfragment@gmail.com>
2021-04-28 21:04:45 -0600

    Auto Updated: Wed 04:21 Apr 28 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -103,4 +103,15 @@ def factorial(n):
         prod *= num
     return prod
 
-print(factorial(5))
+# print(factorial(5))
+
+numbers = [0,1,2,3,4,5,6,7,8,9]
+
+base_ten = []
+
+for dog in numbers:
+    for cat in numbers:
+        base_ten.append([dog, cat])
+
+for num in base_ten:
+    print(num)
