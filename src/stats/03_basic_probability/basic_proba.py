66239a6221dd96ea8d9cb4d8641141c0f6946afc
Tovio Roberts <clownfragment@gmail.com>
2021-04-28 21:05:55 -0600

    Auto Updated: Wed 05:21 Apr 28 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -109,9 +109,11 @@ numbers = [0,1,2,3,4,5,6,7,8,9]
 
 base_ten = []
 
-for dog in numbers:
-    for cat in numbers:
-        base_ten.append([dog, cat])
+for num1 in numbers:
+    for num2 in numbers:
+        for num3 in numbers:
+            for num4 in numbers:
+                base_ten.append([num1, num2, num3, num4])
 
 for num in base_ten:
     print(num)
