89eaba0e605f54170aee268b486850a1ff05b6e7
Tovio Roberts <clownfragment@gmail.com>
2021-04-28 19:13:06 -0600

    Auto Updated: Wed 13:19 Apr 28 2021

--- src/stats/03_basic_probability/axioms_of_probability.py
+++ src/stats/03_basic_probability/axioms_of_probability.py
@@ -1,5 +1,12 @@
 set1 = {'a', 'b', 'c'}
 set2 = {'c', 'd', 'e'}
 
-print(set1.union(set2) == set2.union(set1))
-print(set1.intersection(set2) == set2.intersection(set1))
+# print(set1.union(set2) == set2.union(set1))
+# print(set1.intersection(set2) == set2.intersection(set1))
+
+a = True
+b = False
+
+# print((a or b) == (b or a))
+# print((a and b) == (b and a)) 
+
