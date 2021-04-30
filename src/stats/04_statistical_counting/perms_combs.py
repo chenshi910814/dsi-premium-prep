f371f554ef04f9959a31e62bd0280ac887d07c24
Tovio Roberts <clownfragment@gmail.com>
2021-04-29 19:42:13 -0600

    Auto Updated: Thu 42:19 Apr 29 2021

--- src/stats/04_statistical_counting/perms_combs.py
+++ src/stats/04_statistical_counting/perms_combs.py
@@ -12,10 +12,12 @@ def factorial(num):
 def permutations(n, k):
     return int(factorial(n) / factorial(n-k))
 
+print(permutations(n=30, k=17))
+
 def permutations(n, k):
     perm = 1
     for i in range(n, n-k, -1):
         perm *= i
     return perm
 
-print(permutations(n=5, k=5))
+print(permutations(n=20, k=12))
