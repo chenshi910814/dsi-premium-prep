90e9bbd01b24a6f196934e0d489316a440e7b3ad
Tovio Roberts <clownfragment@gmail.com>
2021-05-04 19:16:10 -0600

    Auto Updated: Tue 16:19 May 04 2021

--- src/stats/05_general_analytic_approach/analytic_approach.py
+++ src/stats/05_general_analytic_approach/analytic_approach.py
@@ -1,7 +1,7 @@
 rolls = range(1, 36+1)
 
 suits = ['Spades', 'Clubs', 'Diamonds', 'Hearts']
-card_nums['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
+card_nums = ['Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace']
 
 cards = []
 
