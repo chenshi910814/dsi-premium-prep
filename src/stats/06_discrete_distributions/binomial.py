4afafd76b03925c57612d1f150d3f34ab2908b98
Tovio Roberts <clownfragment@gmail.com>
2021-05-05 19:47:05 -0600

    Auto Updated: Wed 47:19 May 05 2021

--- src/stats/06_discrete_distributions/binomial.py
+++ src/stats/06_discrete_distributions/binomial.py
@@ -39,5 +39,6 @@ def get_binary(n_bits=8):
     
     return bins_d
 
-for dec, bin_ in get_binary(n_bits=64).items():
-    print(f'{dec}: {bin_}')
+# for dec, bin_ in get_binary(n_bits=64).items():
+#     print(f'{dec}: {bin_}')
+
