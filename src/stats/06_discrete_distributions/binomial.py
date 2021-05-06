6274931ed1d6d3fd855f8f86b45d306242e5c785
Tovio Roberts <clownfragment@gmail.com>
2021-05-05 20:10:10 -0600

    Auto Updated: Wed 10:20 May 05 2021

--- src/stats/06_discrete_distributions/binomial.py
+++ src/stats/06_discrete_distributions/binomial.py
@@ -72,3 +72,9 @@ from random import choice
 def get_bit():
     return choice([0,1])
 
+def generate_n_bits(n=8):
+    return [get_bit() for _ in range(n)]
+    # lst = []
+    # for _ in range(n):
+    #     lst.append(get_bit())
+    # return lst
