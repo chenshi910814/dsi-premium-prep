ad349849d7a455fa10377afac5f5f2e751f88997
Tovio Roberts <clownfragment@gmail.com>
2021-05-05 20:38:43 -0600

    Auto Updated: Wed 38:20 May 05 2021

--- src/stats/06_discrete_distributions/binomial.py
+++ src/stats/06_discrete_distributions/binomial.py
@@ -156,7 +156,32 @@ def binary_sampling_vary_p(num_bits=8, p=0.5, num_samples=1000):
     return d
 
 
-d = binary_sampling_vary_p(num_bits=8, p=0.95, num_samples=1000)
+# d = binary_sampling_vary_p(num_bits=8, p=0.95, num_samples=1000)
 
-for k, cnt in sorted(d.items()):
-    print(f'{k}: {cnt / sum(d.values())}')
+# for k, cnt in sorted(d.items()):
+#     print(f'{k}: {cnt / sum(d.values())}')
+
+
+def binary_sampling_clt(n_bits=8, p=0.5, num_samples=1000, num_sample_trials=500):
+    d_out = dict()
+
+    for _ in range(num_sample_trials):
+        d = binary_sampling_vary_p(n_bits, p, num_samples)
+
+        for k, v in d.items():
+            if k not in d_out:
+                d_out[k] = []
+            d_out[k].append(v)
+
+    for k, v in d_out.items():
+        d_out[k] = sum(v) / len(v)
+    
+    return d_out
+
+# d = binary_sampling_clt(n_bits=8, num_samples=1000, num_sample_trials=500)
+
+# for k, v in sorted(d.items()):
+#     print(f'{k}: {v}')
+
+# for k, v in sorted(d.items()):
+#     print(f'{k}: {v / sum(d.values())}')
