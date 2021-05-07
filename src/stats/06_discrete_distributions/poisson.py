120830f1f1a39710620c3266a79ebbf85993eab2
Tovio Roberts <clownfragment@gmail.com>
2021-05-06 20:27:32 -0600

    Auto Updated: Thu 27:20 May 06 2021

--- src/stats/06_discrete_distributions/poisson.py
+++ src/stats/06_discrete_distributions/poisson.py
@@ -4,3 +4,21 @@ def poisson_pmf(lam, k):
     return (e ** (-lam) * lam**k) / factorial(k)
 
 # print(poisson_pmf(10, 10))
+
+
+def combinations(n, k):
+    return int(factorial(n) / (factorial(n-k) * factorial(k)))
+
+def binomial_pmf(n, k, p=0.5):
+    return combinations(n, k) * (p**k) * (1-p)**(n-k)
+
+
+
+lam = 10
+k = 10
+
+
+for n in range(k, 10000):
+    print(f'binom: {round(binomial_pmf(n, k, p=(lam/n)), 7)}')
+    print(f'poiss: {round(poisson_pmf(lam, k), 7)}')
+    print()
