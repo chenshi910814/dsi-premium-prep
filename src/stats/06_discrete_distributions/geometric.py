f37c7cd91fa4669aa12d3afa17a3c25815bd4857
Tovio Roberts <clownfragment@gmail.com>
2021-05-08 12:09:08 -0600

    Auto Updated: Sat 09:12 May 08 2021

---
+++ src/stats/06_discrete_distributions/geometric.py
@@ -0,0 +1,11 @@
+'''
+You're sitting on a park bench.
+There is a 0.2 probability that any given 
+person walking by is wearing a red hat. What
+is the probability that the first person you
+see wearing a red hat is the 4th person who 
+walks by?
+'''
+p = 0.2
+k_incl = 4
+k_excl = 3
