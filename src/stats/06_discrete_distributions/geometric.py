835b254cea80566c84e21124168cf80f5be79de9
Tovio Roberts <clownfragment@gmail.com>
2021-05-08 13:13:41 -0600

    Auto Updated: Sat 13:13 May 08 2021

--- src/stats/06_discrete_distributions/geometric.py
+++ src/stats/06_discrete_distributions/geometric.py
@@ -74,7 +74,7 @@ def geometric_cdf_dict(p, k_high):
     return d
 
 
-d = geometric_cdf_dict(p=0.5, k_high=10)
+d = geometric_cdf_dict(p=0.5, k_high=1000)
 
 for k, v in d.items():
     print(f'{k}: {v}')
