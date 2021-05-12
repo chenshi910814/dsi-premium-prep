51be554dc1ed1886a2ce2bee04c09970e56b173f
Tovio Roberts <clownfragment@gmail.com>
2021-05-11 20:16:49 -0600

    Auto Updated: Tue 16:20 May 11 2021

--- src/stats/07_continuous_distributions/uniform.py
+++ src/stats/07_continuous_distributions/uniform.py
@@ -58,3 +58,5 @@ def exponential_stdev(lam):
 
 def normal_pdf(x=0, mu=0, sigma=1):
     return (1 / (sigma * sqrt(2 * pi))) * e ** ((-1/2) * ((x-mu) / sigma)**2)
+
+print(normal_pdf(x=0.5, mu=0, sigma=1))
