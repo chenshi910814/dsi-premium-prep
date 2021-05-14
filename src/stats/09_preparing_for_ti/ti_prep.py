8875976c15bddaf7e787664e13276aa6650944b5
Tovio Roberts <clownfragment@gmail.com>
2021-05-13 19:06:09 -0600

    Auto Updated: Thu 06:19 May 13 2021

---
+++ src/stats/09_preparing_for_ti/ti_prep.py
@@ -0,0 +1,7 @@
+'''
+We're observing ships on a bay. We see 5 types of ships: tankers, sailboats, yachts, cruise ships, transport.
+
+There are rules in the bay that tankers cannot follow or precede cruise ships or yachts. Sailboats cannot precede or follow transport ships.
+'''
+
+ships = ['tanker', 'sailboat', 'yacht', 'cruise ship', 'transport ship']
