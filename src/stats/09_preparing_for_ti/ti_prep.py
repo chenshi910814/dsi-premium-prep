1d49fdb9a60ac600af57ddf64c17b0c7c3014e45
Tovio Roberts <clownfragment@gmail.com>
2021-05-13 20:28:24 -0600

    Auto Updated: Thu 28:20 May 13 2021

--- src/stats/09_preparing_for_ti/ti_prep.py
+++ src/stats/09_preparing_for_ti/ti_prep.py
@@ -179,8 +179,8 @@ def bacteria_war_outcomes(bac_1_attr, bac_2_attr, num_samples=1000):
             d['both die  '] += 1
     return d
 
-bac_1 = [6, 8, 9, 12, 2, 1]
-bac_2 = [2, 18, 20, 5, 1, 2]
+bac_1 = [sum([6, 8, 9, 12, 2, 1])]
+bac_2 = [sum([2, 18, 20, 5, 1, 2])]
 
 d = bacteria_war_outcomes(bac_1, bac_2, num_samples=10000)
 
