1004896cb2a8965cd0d214ac2d575652a2eeee44
Tovio Roberts <clownfragment@gmail.com>
2021-05-15 12:22:00 -0600

    Auto Updated: Sat 22:12 May 15 2021

--- src/stats/09_preparing_for_ti/ti_prep.py
+++ src/stats/09_preparing_for_ti/ti_prep.py
@@ -267,3 +267,14 @@ def bins_dict(outcomes_Y):
 
 
 
+def mean(lst, trim=0):
+    lst_ = lst.copy()
+
+    if trim:
+        lst_ = sorted(lst_)[trim:-trim]
+    return sum(lst_) / len(lst_)
+
+
+a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
+
+mean(a, trim=1)
