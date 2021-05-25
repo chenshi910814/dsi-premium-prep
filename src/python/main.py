f7c1d5169f8d199a07190fe36d67bad62ed07e21
Clark Walker <303walker@gmail.com>
2021-05-24 18:57:22 -0600

    update main and day 1 python

---
+++ src/python/main.py
@@ -0,0 +1,29 @@
+# """un/comment to de/activate ###########################
+
+
+
+#######################################################"""
+# """un/comment to de/activate ###########################
+
+
+
+#######################################################"""
+
+def main():
+    print("\n"*20)
+    import day_01_lib
+    # import day_02_lib
+    # import day_03_lib
+    # import day_04_lib
+    # import day_05_lib
+    # import day_06_lib
+    # import day_07_lib
+    # import day_08_lib
+    # import day_09_lib
+    # import day_10_lib
+    # import day_11_lib
+    # import day_12_lib
+    # import day_13_lib
+
+if __name__ == "__main__":
+    main()
