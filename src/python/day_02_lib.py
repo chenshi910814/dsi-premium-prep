40fb69553cad988c2e29794a733b2d20143bd418
Clark Walker <303walker@gmail.com>
2021-05-27 18:41:19 -0600

    finishing day 3

---
+++ src/python/day_02_lib.py
@@ -0,0 +1,133 @@
+# """un/comment to de/activate ###########################
+
+# evaluation patterns
+# types
+# abstractions
+    # variables
+    # functions
+# controll flow
+    # conditions
+# loops
+    # for
+    # while
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+# int, float, bool
+print( int(4.0) )
+print( bool(4.0) )
+print( bool(0.0) )
+print( bool(0) )
+print( bool(-132) )
+# print( int(4.0) )
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+print( 10 /  3 )
+print( 10 // 3 )
+print( 10 % 3 )
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+a, b = 10, 25
+c = (a**2 + b**2)**(1/2)
+print(c)
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+print( 4 == 4 ) # True
+print( 4 == 5 ) # False
+print( 4 != 5 ) # False
+print( 4 != 4 ) # False
+
+print( 4 > 3 ) # True
+print( 4 > 4 ) # False
+print( 4 > 5 ) # False
+
+print( 4 < 3 ) # False
+print( 4 < 4 ) # False
+print( 4 < 5 ) # True
+
+print( 4 >= 3 ) # True
+print( 4 >= 4 ) # True
+print( 4 >= 5 ) # False
+
+print( 4 <= 3 ) # False
+print( 4 <= 4 ) # True
+print( 4 <= 5 ) # True
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+print( True  and True  ) # True
+print( False and True  ) # False
+print( True  and False ) # False
+print( False and False ) # False
+
+print( True  or  True  ) # True
+print( False or  True  ) # True
+print( True  or  False ) # True
+print( False or  False ) # False
+
+print( not True  ) # False
+print( not False ) # True
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+a = 4
+b = 27
+
+print( (a > b) or ((a**2) > (b / 2)) )
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+thing = (not (not (True or False) and (True and True))) and True or True
+print( thing )
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+print( True  != True  ) # False
+print( False != True  ) # True
+print( True  != False ) # True
+print( False != False ) # False
+
+#######################################################"""
+"""un/comment to de/activate ###########################
+
+a = 100
+b = 20
+if a < b:
+    print("a is")
+    print("greater")
+    print("than b")
+elif a == b:
+    print("a is greater than b")
+else:
+    print("a is greater than b")
+
+print("fin")
+
+#######################################################"""
+# """un/comment to de/activate ###########################
+
+# Question: print the division of the result of 4 minus 2 times 2 into twenty
+# Clark's Answer: print ((4-2) * (20/2))
+# Sherry sees print(  20 / ( 4 - 2 * 2) )
+# this would give a Division by Zero;
+# I would add more parentheses if I saw more commas in the problem statement; Seems like the problem is poorly stated. --Does anyone else see this discrepancy?
+
+
+
+#######################################################"""
+# """un/comment to de/activate ###########################
+
+
+
+#######################################################"""
--- src/python/main.py
+++ src/python/main.py
@@ -11,8 +11,8 @@
 
 def main():
     print("\n"*20)
-    import day_01_lib
-    # import day_02_lib
+    # import day_01_lib
+    import day_02_lib
     # import day_03_lib
     # import day_04_lib
     # import day_05_lib
