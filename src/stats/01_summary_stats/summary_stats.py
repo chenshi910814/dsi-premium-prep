6944fe3ad67c939d338d62f1dcf5b25debd5f54a
Tovio Roberts <clownfragment@gmail.com>
2021-06-05 13:54:03 -0600

    Auto Updated: Sat 54:13 Jun 05 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -64,7 +64,7 @@ home_sales = [590, 615, 575, 608, 350, 1285, 408, 540, 555, 679]
 
 
 
-pets = ['cat', 'dog', 'goldfish', 'ferret', 'ferret', 'cat', 'cat']
+pets = ['cat', 'dog', 'goldfish', 'ferret', 'ferret', 'cat', 'cat'] * 5
 
 
 print(pets.count('ferret'))
