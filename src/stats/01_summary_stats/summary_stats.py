eaeb11b1c2cd0bb37b1ec79e9643fcae687e67c7
Tovio Roberts <clownfragment@gmail.com>
2021-06-05 13:58:19 -0600

    Auto Updated: Sat 58:13 Jun 05 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -72,7 +72,7 @@ def mode(lst):
     most_occurring = lst[0]
 
     for item in lst[1:]:
-        if lst.count(item) > lst.count(most_occurring):
+        if lst.count(item) >= lst.count(most_occurring):
             most_occurring = item
     
     return most_occurring
