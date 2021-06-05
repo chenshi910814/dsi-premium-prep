413c52eab261dc25c54c7fcea5d6c5661a2af86e
Tovio Roberts <clownfragment@gmail.com>
2021-06-05 12:51:05 -0600

    Auto Updated: Sat 51:12 Jun 05 2021

--- src/stats/01_summary_stats/summary_stats.py
+++ src/stats/01_summary_stats/summary_stats.py
@@ -14,4 +14,10 @@ a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 urban = [6.0, 5.0, 11.0, 33.0, 4.0, 5.0, 80.0, 18.0, 35.0, 17.0, 23.0]
 farmhouse = [4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0, 2.0, 0.3]
 
-print(round(mean(urban), 1))
+print(f'Sample Mean Urban: {round(mean(urban), 1)}')
+print(f'Sample Mean Farmhouse: {round(mean(farmhouse), 1)}')
+
+print()
+
+print(f'Trimmed Sample Mean Urban: {round(mean(urban, trim_by=1), 1)}')
+print(f'Trimmed Sample Mean Farmhouse: {round(mean(farmhouse, trim_by=1), 1)}')
