801ab792eb60e685ec1b058c225f154d6ac9289e
Tovio Roberts <clownfragment@gmail.com>
2021-06-15 20:55:35 -0600

    Auto Updated: Tue 55:20 Jun 15 2021

--- src/stats/03_basic_probability/axioms_of_probability.py
+++ src/stats/03_basic_probability/axioms_of_probability.py
@@ -4,3 +4,9 @@ set2 = {'c', 'd', 'e'}
 # commutative
 print(set1.union(set2) == set2.union(set1))
 print(set1.intersection(set2) == set2.intersection(set1))
+
+a = True
+b = False
+
+print( (a or b) == (b or a) )
+print( (a and b) == (b and a) )
