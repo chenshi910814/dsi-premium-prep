ced5fde9c262eff4c05080b439f4ac2640a4036f
Tovio Roberts <clownfragment@gmail.com>
2021-06-16 19:56:34 -0600

    Auto Updated: Wed 56:19 Jun 16 2021

--- src/stats/03_basic_probability/basic_proba.py
+++ src/stats/03_basic_probability/basic_proba.py
@@ -15,4 +15,19 @@ for pet_outcome in pet_outcomes:
     if pet_outcome.count('cat') >= 2:
         two_or_more_cats.append(pet_outcome)
 
-print(len(two_or_more_cats) / len(pet_outcomes))
+# print(len(two_or_more_cats) / len(pet_outcomes))
+
+from random import choice
+
+def coin_flip():
+    flip = ['H', 'T']
+    return choice(flip)
+
+def series_of_flips(n):
+    flips = []
+    for _ in range(n):
+        flips.append(coin_flip())
+    return flips
+
+
+print(series_of_flips(n=12))
