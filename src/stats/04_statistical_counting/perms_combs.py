0840611dc660c9d2e342711bd29d0e9359c3f1fb
Tovio Roberts <clownfragment@gmail.com>
2021-06-17 20:33:45 -0600

    Auto Updated: Thu 33:20 Jun 17 2021

--- src/stats/04_statistical_counting/perms_combs.py
+++ src/stats/04_statistical_counting/perms_combs.py
@@ -156,8 +156,18 @@ def basketball_combs():
         if len(list(set(five))) == 5:
             perms.append(five)
 
+    # for five in perms:
+    #     print(five)
+
+    combs = []
+
     for five in perms:
-        print(five)
+        sorted_five = sorted(five)
+
+        if sorted_five not in combs:
+            combs.append(sorted_five)
+
+    return combs
 
 
 basketball_perms = basketball_combs()
