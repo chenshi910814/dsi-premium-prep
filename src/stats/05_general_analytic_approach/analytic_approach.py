88126d7f4b72a3fbaec4aef2c61724f8e1c6bd48
Tovio Roberts <clownfragment@gmail.com>
2021-06-21 19:25:40 -0600

    Auto Updated: Mon 25:19 Jun 21 2021

--- src/stats/05_general_analytic_approach/analytic_approach.py
+++ src/stats/05_general_analytic_approach/analytic_approach.py
@@ -8,5 +8,19 @@ for suit in suits:
     for card in card_nums:
         cards.append(f'{card} of {suit}')
 
-for card in cards:
-    print(card)
+# for card in cards:
+#     print(card)
+
+coin_flips = []
+for flip1 in ['T', 'H']:
+    for flip2 in ['T', 'H']:
+        for flip3 in ['T', 'H']:
+            coin_flips.append([flip1, flip2, flip3])
+
+S = []
+for roll in rolls:
+    for card in cards:
+        for flip in coin_flips:
+            S.append([roll, card, flip])
+
+            
