4f6ebb273387e17a1191243537123884ef350ab9
Tovio Roberts <clownfragment@gmail.com>
2021-06-21 20:24:22 -0600

    Auto Updated: Mon 24:20 Jun 21 2021

--- src/stats/05_general_analytic_approach/analytic_approach.py
+++ src/stats/05_general_analytic_approach/analytic_approach.py
@@ -89,3 +89,8 @@ def gumball_capacity():
 
     for r in range(4, 100+1):
         d[r] = round(spherical_volume(r) / spherical_volume(1))
+
+    return d
+
+for r, v in gumball_capacity().items():
+    print(f'{r}: {v}')
