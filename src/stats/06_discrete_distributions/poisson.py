cf7750bc9a625fb9d16e0b728c41fe0427b99164
Tovio Roberts <clownfragment@gmail.com>
2021-06-23 20:51:02 -0600

    Auto Updated: Wed 51:20 Jun 23 2021

---
+++ src/stats/06_discrete_distributions/poisson.py
@@ -0,0 +1,12 @@
+from math import e
+
+def factorial(n):
+    prod = 1
+    for num in range(2, n+1):
+        prod *= num
+    return prod
+
+
+def poisson_pmf(lam, k):
+    return lam**k * e**(-lam) / factorial(k)
+
