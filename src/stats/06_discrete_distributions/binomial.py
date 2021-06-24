e965c0c477d5f81facdcc08ffc2d954bec22ce9c
Tovio Roberts <clownfragment@gmail.com>
2021-06-23 20:32:44 -0600

    Auto Updated: Wed 32:20 Jun 23 2021

--- src/stats/06_discrete_distributions/binomial.py
+++ src/stats/06_discrete_distributions/binomial.py
@@ -306,7 +306,7 @@ def binomial_cdf_dict(n, k_low, k_high, p=0.5):
         d[k] = binomial_cdf(n, k, p)
     return d
 
-d = binomial_pmf_dict(n=8, k_low=0, k_high=8, p=0.25)
+d = binomial_cdf_dict(n=8, k_low=0, k_high=8, p=0.25)
 
 for k, v in d.items():
     print(f'{k}: {v}')
