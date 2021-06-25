92a1520bca1d264953f90e1e9a41841997cce779
Tovio Roberts <clownfragment@gmail.com>
2021-06-24 21:02:47 -0600

    Auto Updated: Thu 02:21 Jun 24 2021

--- src/stats/07_continuous_distributions/exponential.py
+++ src/stats/07_continuous_distributions/exponential.py
@@ -19,8 +19,8 @@ def exponential_stdev(lam):
 
 
 
-print(1 - exponential_cdf(lam=0.1, x=10)) # 0.368
+# print(1 - exponential_cdf(lam=0.1, x=10)) # 0.368
 
-print(exponential_mean(lam=0.1))
+# print(exponential_mean(lam=0.1))
 
-print(exponential_stdev(lam=0.1))
+# print(exponential_stdev(lam=0.1))
