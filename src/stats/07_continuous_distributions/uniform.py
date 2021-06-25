22dfabf7ac69c57cfe45e8baec0b37c6fa11ea12
Tovio Roberts <clownfragment@gmail.com>
2021-06-24 20:33:23 -0600

    Auto Updated: Thu 33:20 Jun 24 2021

--- src/stats/07_continuous_distributions/uniform.py
+++ src/stats/07_continuous_distributions/uniform.py
@@ -10,4 +10,4 @@ binary = [1,1,1,1,1,1,1,1]#get_binary(n=8)
 
 accum = 0.0
 
-print(binary[0] * 0.5**1 + binary[1] * 0.5**2 + binary[2] * 0.5**2 + binary[3] * 0.5**2 + binary[4] * 0.5**2 + binary[5] * 0.5**2 + binary[6] * 0.5**2 + binary[7] * 0.5**2)
+print(binary[0] * 0.5**1 + binary[1] * 0.5**2 + binary[2] * 0.5**3 + binary[3] * 0.5**4 + binary[4] * 0.5**5 + binary[5] * 0.5**6 + binary[6] * 0.5**7 + binary[7] * 0.5**8)
