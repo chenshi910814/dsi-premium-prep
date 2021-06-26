c7e96761b03e03d1e64bffcd1f4a8c82f9b176b7
Tovio Roberts <clownfragment@gmail.com>
2021-06-26 12:42:17 -0600

    Auto Updated: Sat 42:12 Jun 26 2021

--- src/stats/07_continuous_distributions/normal.py
+++ src/stats/07_continuous_distributions/normal.py
@@ -8,3 +8,16 @@ def normal_pdf(x=0, mu=0, sigma=1):
 
 
 
+def normal_cdf(x=0, mu=0, sigma=1):
+    x_width = 0.001
+    x_vals = [num*x_width for num in range(-1000, int(x*1000))]
+    accum = 0.0
+
+    for x in x_vals:
+        accum += normal_pdf(x, mu, sigma) * x_width
+        if x > x:
+            break
+    return accum
+
+print(normal_cdf(x=300, mu=475, sigma=98))
+
