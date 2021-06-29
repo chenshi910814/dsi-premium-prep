b65436d4d61e6cba32e8b0cc1678b4aedbda7823
Tovio Roberts <clownfragment@gmail.com>
2021-06-28 19:41:07 -0600

    Auto Updated: Mon 41:19 Jun 28 2021

--- src/stats/09_preparing_for_ti/ti_prep.py
+++ src/stats/09_preparing_for_ti/ti_prep.py
@@ -24,3 +24,11 @@ def variance(lst, sample=True):
 # print(variance(a)**(1/2))
 
 
+def factorial(n):
+    prod = 1
+    for num in range(1, n+1):
+        prod *= num
+    return prod
+
+
+print(factorial(5))
