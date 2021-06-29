37b809b5d52874d8732278e37d88de976dbb1e96
Tovio Roberts <clownfragment@gmail.com>
2021-06-28 19:36:50 -0600

    Auto Updated: Mon 36:19 Jun 28 2021

--- src/stats/09_preparing_for_ti/ti_prep.py
+++ src/stats/09_preparing_for_ti/ti_prep.py
@@ -12,3 +12,13 @@ a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
 # print(mean(a, trim=2))
 
 
+def variance(lst, sample=True):
+    mean_ = mean(lst)
+    total = 0.0
+
+    for item in lst:
+        total += (item - mean_)**2
+
+    return total / (len(lst) - sample)
+
+print(variance(a))
